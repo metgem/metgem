03cb7070fb1db9d7bea2314db6a3a86dedd6919f
Nicolas ELIE <nicolas.elie@cnrs.fr>
2018-02-15 11:25:23 +0100

    Added filter to allow only MGF files in the 'open file to process' dialog

--- lib/ui/open_file_dialog.py
+++ lib/ui/open_file_dialog.py
@@ -41,6 +41,8 @@ class OpenFileDialog(OpenFileDialogBase, OpenFileDialogUI):
     def browse(self, type='process'):
         dialog = QFileDialog(self)
         dialog.setOption(QFileDialog.ShowDirsOnly)
+        if type == 'process':
+            dialog.setNameFilters(["MGF Files (*.mgf)", "All files (*.*)"])
         if dialog.exec_() == QDialog.Accepted:
             filename = dialog.selectedFiles()[0]
             if type == 'process':
