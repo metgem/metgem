ac56cfbbb72a00f88cfe553b09a6d7d3b6545b91
Nicolas ELIE <nicolas.elie@cnrs.fr>
2018-03-31 00:50:59 +0200

    Code clean up

--- lib/workers/read_metadata.py
+++ lib/workers/read_metadata.py
@@ -1,16 +1,14 @@
 import pandas as pd
 
-
 from .base import BaseWorker
 
     
 class ReadMetadataWorker(BaseWorker):
     
-    def __init__(self, filename, csv_separator = None):
+    def __init__(self, filename, csv_separator=None):
         super().__init__()
         self.filename = filename
         self.csv_separator = csv_separator
-
 
     def run(self):
         if self.filename.endswith(".csv"):
