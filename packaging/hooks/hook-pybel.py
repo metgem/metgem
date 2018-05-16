37ff4ffc68eb5cc280e78b56f74432fbaa27e65e
Nicolas ELIE <nicolas.elie@cnrs.fr>
2018-05-16 21:26:42 +0200

    Fix missing inchi.dll and cairo.dll

--- packaging/hooks/hook-pybel.py
+++ packaging/hooks/hook-pybel.py
@@ -1,3 +1,6 @@
 from PyInstaller.utils.hooks import collect_data_files, get_module_file_attribute
 
-datas = collect_data_files('openbabel') + [(get_module_file_attribute('openbabel._openbabel'), 'openbabel')]
+datas = [(get_module_file_attribute('openbabel._openbabel'), 'openbabel')]
+d = collect_data_files('openbabel')
+datas += d
+datas += [(x, '') for x, _ in d if x.endswith('.dll')]
