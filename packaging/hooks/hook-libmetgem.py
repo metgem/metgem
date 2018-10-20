c3327f3ae1e4f1058f7bb21c5b9f3174f5484cc6
Nicolas ELIE <nicolas.elie@cnrs.fr>
2018-10-20 17:22:01 +0200

    Update PyInstaller's hook for libmetgem

--- packaging/hooks/hook-libmetgem.py
+++ packaging/hooks/hook-libmetgem.py
@@ -1,1 +1,4 @@
-hiddenimports = ['libmetgem.common']
+hiddenimports = ['libmetgem.common', 'libmetgem._common',
+                 'libmetgem._cosine', 'libmetgem._database',
+                 'libmetgem._filer', 'libmetgem._mgf',
+                 'libmetgem._network']
