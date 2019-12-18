f9ed60c72fb94eda2f7bf7a9f99c8153d10bf858
Nicolas ELIE <nicolas.elie@cnrs.fr>
2019-12-18 11:33:23 +0100

    Fix Isomap worker

--- lib/workers/embedding/isomap.py
+++ lib/workers/embedding/isomap.py
@@ -1,10 +1,7 @@
-import io
-
 from sklearn.manifold import Isomap
 
 from .base import EmbeddingWorker
 from ...utils import AttrDict
-from ...errors import UserRequestedStopError
 
 
 class IsomapVisualizationOptions(AttrDict):
@@ -18,31 +15,6 @@ class IsomapVisualizationOptions(AttrDict):
                          max_iter=300)
 
 
-class ProgressStringIO(io.StringIO):
-
-    def __init__(self, parent, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.parent = parent
-        self._last_value = 0
-        self._shift = 0
-
-    def write(self, s):
-        if self.parent.isStopped():
-            raise UserRequestedStopError()
-        # elif 'it: ' in s:
-        #     s = s.split('it: ')[1]
-        #     if ', stress ' in s:
-        #         s = s.split(', stress ')[0]
-        #     try:
-        #         value = int(s)
-        #         if value < self._last_value:
-        #             self._shift += self._last_value
-        #         self._last_value = value
-        #         self.parent.updated.emit(int((self._shift + value) / self._n_init))
-            # except ValueError:
-            #     pass
-
-
 class IsomapWorker(EmbeddingWorker):
 
     # noinspection PyAttributeOutsideInit
@@ -50,8 +22,6 @@ class IsomapWorker(EmbeddingWorker):
         self._estimator = Isomap(max_iter=self.options.max_iter,
                                  n_neighbors=self.options.n_neighbors,
                                  metric='precomputed')
-
-        self.max = self.options.max_iter
+        self.max = 0
         self.iterative_update = False
         self.desc = 'MDS: Iteration {value:d} of {max:d}'
-        self._io_wrapper = ProgressStringIO(self)
