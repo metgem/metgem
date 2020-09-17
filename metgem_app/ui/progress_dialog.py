8cc631f73cd61896c3e5063bbcdd080016249062
Nicolas ELIE <nicolas.elie@cnrs.fr>
2020-09-17 10:07:58 +0200

    Use QtWinExtras to show progress if available

--- metgem_app/ui/progress_dialog.py
+++ metgem_app/ui/progress_dialog.py
@@ -3,6 +3,13 @@ import os
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
 
+try:
+    # noinspection PyUnresolvedReferences
+    from PyQt5.QtWinExtras import QWinTaskbarButton, QWinTaskbarProgress
+    HAS_WINEXTRAS = True
+except ImportError:
+    HAS_WINEXTRAS = False
+
 UI_FILE = os.path.join(os.path.dirname(__file__), 'progress_dialog.ui')
 
 ProgressDialogUI, ProgressDialogBase = uic.loadUiType(UI_FILE,
@@ -17,12 +24,18 @@ class ProgressDialog(ProgressDialogUI, ProgressDialogBase):
 
         self.setupUi(self)
         self.setWindowFlags(Qt.Tool | Qt.CustomizeWindowHint | Qt.WindowCloseButtonHint)
+        if HAS_WINEXTRAS:
+            self._button = QWinTaskbarButton(self)
+            self._button.setWindow(self.windowHandle())
+            self._button.setOverlayIcon(self.icon())
 
     def value(self):
         return self.progressBar.value()
 
     def setValue(self, value):
         self.progressBar.setValue(value)
+        if HAS_WINEXTRAS:
+            self._button.progress().setValue(value)
 
     def format(self):
         return self.windowTitle()
@@ -33,17 +46,31 @@ class ProgressDialog(ProgressDialogUI, ProgressDialogBase):
     def minimum(self):
         return self.progressBar.minimum()
 
-    def setMinimum(self, value):
-        self.progressBar.setMinimum(value)
+    def setMinimum(self, minimum):
+        self.progressBar.setMinimum(minimum)
+        if HAS_WINEXTRAS:
+            self._button.progress().setMinimum(minimum)
 
     def maximum(self):
         return self.progressBar.maximum()
 
-    def setMaximum(self, value):
-        self.progressBar.setMaximum(value)
+    def setMaximum(self, maximum):
+        self.progressBar.setMaximum(maximum)
+        if HAS_WINEXTRAS:
+            self._button.progress().setMaximum(maximum)
+
+    def setRange(self, minimum, maximum):
+        self.progresBar.setRange(minimum, maximum)
+        if HAS_WINEXTRAS:
+            self._button.progress().setRange(minimum, maximum)
 
     def reject(self):
         return super().reject()
 
     def done(self, r: int):
         return super().done(r)
+
+    def show(self):
+        super().show()
+        if HAS_WINEXTRAS:
+            self._button.progress().show()
