da2d36897987df82f536d4fddf1b368cc1a9d5db
Nicolas Elie <nicolas.elie@cnrs.fr>
2022-02-10 13:11:36 +0100

    Add command line interface script

--- metgem_app/utils/qt.py
+++ metgem_app/utils/qt.py
@@ -3,16 +3,26 @@ try:
     from PyQt5.QtCore import Qt
     from PyQt5.QtGui import QColor
 except (ImportError, RuntimeError):
+    from typing import Callable
+
     class QObject(object):
         pass
 
     class pyqtSignal(object):
+        slots = []
+
         def __init__(self, *args):
             super().__init__()
+
+        def connect(self, callable: Callable):
+            self.slots.append(callable)
 
         def emit(self, *args, **kwargs):
             if isinstance(args, (list, tuple)) and args and isinstance(args[0], Exception):
                 raise args[0]
+
+            for callable in self.slots:
+                callable(*args, **kwargs)
 
     class Qt(object):
         Horizontal = 1
