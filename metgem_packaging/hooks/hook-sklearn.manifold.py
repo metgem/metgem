05ef5b2127a2a643f06196da86c43ba886aba3a8
Nicolas ELIE <nicolas.elie@cnrs.fr>
2022-03-24 13:02:24 +0100

    Add PyInstaller hidden imports for sklearn.manifold

--- metgem_packaging/hooks/hook-sklearn.manifold.py
+++ metgem_packaging/hooks/hook-sklearn.manifold.py
@@ -1,6 +1,7 @@
 hiddenimports = ['scipy._lib.messagestream',
                  'sklearn.neighbors.typedefs',
                  'sklearn.neighbors.quad_tree',
+                 'sklearn.neighbors._partition_nodes',
                  'sklearn.tree._utils',
                  'sklearn.tree._criterion',
                  'sklearn.tree._splitter',
