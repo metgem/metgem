40eb294a3a10a1fea780f1d22e4ea8c610c6b9da
Nicolas ELIE <nicolas.elie@cnrs.fr>
2022-05-20 10:58:32 +0200

    Update hook for sklearn.utils

--- metgem_packaging/hooks/hook-sklearn.utils.py
+++ metgem_packaging/hooks/hook-sklearn.utils.py
@@ -1,3 +1,4 @@
 hiddenimports = ['sklearn.utils._cython_blas',
                  'sklearn.utils._weight_vector',
-                 'sklearn.utils._typedefs']
+                 'sklearn.utils._typedefs',
+                 'sklearn.utils._heap']
