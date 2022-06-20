c1e2783f49d0edbfb578605284601acc8c928a93
Nicolas ELIE <nicolas.elie@cnrs.fr>
2022-06-20 09:58:23 +0200

    Update ISDB plugin (fixes #108)

--- metgem_app/plugins/isdb.py
+++ metgem_app/plugins/isdb.py
@@ -1,19 +1,35 @@
 """MetGem plugin to download ISDB"""
 
-__version__ = '1.0'
+__version__ = '1.1'
 __description__ = "MetGem plugin to download ISDB"
 __author__ = "Nicolas Elie"
 __email__ = "nicolas.elie@cnrs.fr"
-__copyright__ = "Copyright 2019, CNRS/ICSN"
+__copyright__ = "Copyright 2019-2022, CNRS/ICSN"
 __license__ = "GPLv3"
+
+import json
 
 
 # noinspection PyUnresolvedReferences
 class ISDB(DbSource):
 
     name = "ISDB"
-    items_base_url = "https://raw.githubusercontent.com/oolonek/ISDB/master/Data/dbs/"
+    page = "https://doi.org/10.5281/zenodo.5607185"
+    items_base_url = ""
 
-    def get_items(self):
-        yield "In-Silico Database", [f'UNPD_ISDB_R_p0{i}.mgf' for i in range(1, 10)],\
-              'A database of In-Silico predicted MS/MS spectrum of Natural Products'
+    def get_items(self, tree):
+        json_s = tree.find('.//script[@type="application/ld+json"]').text
+        json_load = json.loads(json_s)
+        description = json_load['description']
+        identifier = json_load['identifier']
+        version = json_load['version']
+        date = json_load['datePublished']
+
+        hrefs = []
+
+        for distrib in json_load['distribution']:
+            if distrib['@type'] == 'DataDownload':
+                href = distrib['contentUrl']
+            hrefs.append(href)
+        yield "In-Silico Database", hrefs,\
+              f"Version {version} ({date}) <a href='{identifier}'>{identifier}</a>\n{description}"
