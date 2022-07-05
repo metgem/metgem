80e76b2a83da4c36f5800e9b83ef7b3bf27222c4
Nicolas ELIE <nicolas.elie@cnrs.fr>
2022-07-05 17:38:35 +0200

    Prevent UI to become unresponsive when a progress dialog is visible

--- metgem_app/ui/progress_dialog.py
+++ metgem_app/ui/progress_dialog.py
@@ -1,5 +1,5 @@
 from qtpy.QtCore import Qt
-from qtpy.QtWidgets import QDialog
+from qtpy.QtWidgets import QDialog, QApplication
 
 try:
     # noinspection PyUnresolvedReferences
@@ -31,6 +31,7 @@ class ProgressDialog(QDialog, Ui_Dialog):
         self.progressBar.setValue(val)
         if HAS_WINEXTRAS:
             self._button.progress().setValue(val)
+        QApplication.processEvents()
 
     def format(self):
         return self.windowTitle()
