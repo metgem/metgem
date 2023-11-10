a2c24106bac6e0fc7864e49171128cb4856c02c3
Nicolas ELIE <nicolas.elie@cnrs.fr>
2023-11-10 08:52:28 +0100

    Updated docs config

--- docs/conf.py
+++ docs/conf.py
@@ -19,14 +19,14 @@ sys.path.insert(0, os.path.abspath('_ext'))
 
 project = 'MetGem'
 # noinspection PyShadowingBuiltins
-copyright = '2020-2021, Nicolas Elie'
+copyright = '2020-2023, Nicolas Elie'
 author = 'Nicolas Elie'
 
 # The short X.Y version
-version = '1.3'
+version = '1.4'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.0'
+release = '1.4.2'
 
 
 # -- General configuration ---------------------------------------------------
